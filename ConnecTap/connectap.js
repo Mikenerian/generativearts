f557b67ae25d9e3cda0247a351b4acdaf8e3f2a7
Mikenerian <rainbowsteam8@gmail.com>
2023-03-30 09:46:09 +0900

    ConnecTap v1

--- ConnecTap/connectap.js
+++ ConnecTap/connectap.js
@@ -1,5 +1,6 @@
 
 let t = 0;
+let u = 77;
 let mousePositions = [];
 let circlePositions = [];
 let maxStoreFrames = 100;
@@ -12,7 +13,7 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(245);
 
   // Store current mouse position
   mousePositions.push({x: mouseX, y: mouseY});
@@ -20,21 +21,30 @@ function draw() {
     mousePositions.shift();
   }
 
+  // Set circle position
   let dividedX = circlePositions[circlePositions.length - 1].x + (mouseX - circlePositions[circlePositions.length - 1].x) / delayRate;
   let dividedY = circlePositions[circlePositions.length - 1].y + (mouseY - circlePositions[circlePositions.length - 1].y) / delayRate;
 
-  // Draw the circle with perlin noise
-  let circleColor = color(0, 180, 255, 100); // R, G, B, A(透明度)
-  fill(circleColor);
-  let circleBase = 200;
-  let noiseValue2 = noise(t + 1000);
-  let sizeVariation = circleBase * (noiseValue2 * 2 - 1) * 0.3;
-  let circleSize = circleBase + sizeVariation;
+  // Draw circles with perlin noise
+  noStroke();
+  for (let i = 0; i < 30; i++) {
+    let c = map(sin(t), -1, 1, 100, 200); // Red value changes with time
+    let a = 80 + i * 3; // Alpha value
+    let circleColor = color(c, c + 10, c + 20, a);
+    fill(circleColor);
+    let circleBase = 200 - i * 1.5;
+    let noiseValueX = noise(t + 1000);
+    let noiseValueY = noise(u + 1000);
+    let sizeVariationX = circleBase * (noiseValueX * 2 - 1) * 0.1;
+    let sizeVariationY = circleBase * (noiseValueY * 2 - 1) * 0.1;
+    let circleSizeX = circleBase + sizeVariationX - i * 2;
+    let circleSizeY = circleBase + sizeVariationY - i * 2;
 
-  if (dividedX && dividedY) {
-    ellipse(dividedX, dividedY, circleSize, circleSize);
-  } else {
-    ellipse(mouseX, mouseY, circleSize, circleSize);
+    if (dividedX && dividedY) {
+      ellipse(dividedX, dividedY, circleSizeX, circleSizeY);
+    } else {
+      ellipse(mouseX, mouseY, circleSizeX, circleSizeY);
+    }
   }
 
   // Store current circle position
@@ -43,6 +53,7 @@ function draw() {
     circlePositions.shift();
   }
 
-  // update noise seed
+  // Update noise seed
   t += 0.01;
+  u += 0.01;
 }
